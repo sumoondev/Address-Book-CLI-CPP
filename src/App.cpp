a24209b0f337a18d8a48b16436eac46ace9daaad
Sumoon Byanjankar <sumoon.dev@gmail.com>
2025-07-24 12:23:11 +0000

    Create: Folder Structure

---
+++ src/App.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+
+// Contact node for AVL Tree
+class Contact {
+
+};
+
+// For performing vital operations like add contact, delete contact, etc;
+class Service {
+
+};
+
+// Object to store in JSON file
+class DataContainer {
+
+};
+
+// For performing utility operations like clear screen, load and save contact, etc.
+class Utility {
+    
+};
+
+// Main section that executes
+int main() {
+
+    return 0;
+}
