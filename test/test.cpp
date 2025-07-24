8e56b5e67be8418456077d0cb31da11a07b911a5
Sumoon Byanjankar <sumoon.dev@gmail.com>
2025-07-24 13:05:21 +0000

    test

--- test/test.cpp
+++ test/test.cpp
@@ -3,7 +3,20 @@
 
 using namespace std;
 
+void clearScreen() {
+    // Check the operating system
+    #ifdef _WIN32
+        system("cls"); // Clear screen for Windows
+    #else
+        system("clear"); // Clear screen for Unix/Linux/Mac
+    #endif
+}
 int main() {
-
+    cout << "This is some text." << endl;
+    cout << "Clearing the screen..." << endl;
+    // Call the function to clear the screen
+    clearScreen();
+    cout << "The screen has been cleared!" << endl;
+    cin;
     return 0;
 }
