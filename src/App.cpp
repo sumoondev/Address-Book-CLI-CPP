c522b8cff8c1bf6cb28f2bbab82cb876f71aa705
Sumoon Byanjankar <sumoon.dev@gmail.com>
2025-07-24 14:38:24 +0000

    Add compareTo() fn & data and constructor in DataContainer

--- src/App.cpp
+++ src/App.cpp
@@ -1,12 +1,14 @@
 #include <iostream>     // perform io operations
 #include <cstdlib>      // for system() fn
 #include <cstring>      // perform string operation
+#include <chrono>       // for chrono::millisecond() fn
+#include <thread>       // for this_thread::sleep_for() fn
 
 using namespace std;
 
 // Contact node for AVL Tree
 class Contact {
-    private:
+    private:    
         string name;
         long mobile;
         string address;
@@ -14,8 +16,8 @@ class Contact {
     public:
         Contact* left;
         Contact* right;
-        static int count;
-        int height;
+        static int count;       // stores count of contacts
+        int height;             // for storing height of current node (for AVL implementation)
 
         Contact(string name, long mobile, string address, string email) {
             this->name = name;
@@ -61,32 +63,58 @@ class Contact {
         string getEmail() {
             return this->email;
         }
-};
 
-// For performing vital operations like add contact, delete contact, etc;
-class Service {
+        int compareTo(Contact c2) {
+            return this->name.compare(c2.name);
+        }
 
+        int compareTo(string name) {
+            return this->name.compare(name);
+        }
+
+        bool compareToUtil(string test) {
+            return this->name.substr(0,test.length()).compare(test);
+        }
 };
 
 // Object to store in JSON file
 class DataContainer {
+    public: 
+        Contact* contact;
+        int count;
+
+        DataContainer(Contact* root, int count) {
+            contact = root;
+            this->count = count;
+        }
+};
+
+// For performing vital operations like add contact, delete contact, etc;
+class Service {
+    private:
+        static Contact* root;
+
 
 };
+
 
 // For performing utility operations like clear screen, load and save contact, etc.
 class Utility {
     private:
 
     public: 
-    static void clearScreen() {
-        // Check the operating system
-        #ifdef _WIN32
-            system("cls"); // Clear screen for Windows
-        #else
-            system("clear"); // Clear screen for Unix/Linux/Mac
-        #endif
-    } 
+        static void clearScreen() {
+            // Check the operating system
+            #ifdef _WIN32
+                system("cls"); // Clear screen for Windows
+            #else
+                system("clear"); // Clear screen for Unix/Linux/Mac
+            #endif
+        } 
+        
+        static void printInvalid() {
 
+        }
 };
 
 // Main section that executes
